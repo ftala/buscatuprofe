c3565ad1fddacb445d60bca8a178aabf4b7516c6
Diego <diaguilar@alumnos.uai.cl>
2017-06-21 22:16:01 -0400

    lito ql vali callampa x5.1

--- resources/views/social.blade.php
+++ resources/views/social.blade.php
@@ -87,6 +87,6 @@
     <div class="fb-page" data-href="https://www.facebook.com/Buscatuprofe-822721104559924/" data-tabs="timeline" data-width="500" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
     <blockquote cite="https://www.facebook.com/Buscatuprofe-822721104559924/" class="fb-xfbml-parse-ignore">
         <a href="https://www.facebook.com/Buscatuprofe-822721104559924/">Buscatuprofe</a></blockquote></div>
-</pre>>
+</pre>
 </body>
 </html>
